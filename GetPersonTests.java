24eb87f82ec49f013404ed3211bd387dfda9b162
lyilaf <102349365+lyilaf@users.noreply.github.com>
2024-12-17 00:36:22 +0500

    Add files via upload

--- GetPersonTests.java
+++ GetPersonTests.java
@@ -24,7 +24,7 @@ public class GetPersonTests extends IntegrationTest {
     RestTemplate restTemplate = new RestTemplate();
 
     @Test
-    @DisplayName("РџРѕР»СѓС‡РµРЅРёРµ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ РїРѕ СЃСѓС‰РµСЃС‚РІСѓСЋС‰РµРјСѓ id")
+    @DisplayName("Получение пользователя по существующему id")
     @AllureId("1")
     public void GetPersonWithId() {
 
@@ -32,7 +32,7 @@ public class GetPersonTests extends IntegrationTest {
 
         String userName = "Alpha";
 
-        step("РЎРѕР·РґР°РµРј РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ СЃ РїРѕРјРѕС‰СЊСЋ РјРµС‚РѕРґР° POST /person СЃ РїР°СЂР°РјРµС‚СЂР°РјРё: name = '" + userName + "'", () -> {
+        step("Создаем пользователя с помощью метода POST /person с параметрами: name = '" + userName + "'", () -> {
             Person personToCreate = new Person(userName);
 
             HttpHeaders headers = new HttpHeaders();
@@ -46,14 +46,14 @@ public class GetPersonTests extends IntegrationTest {
                     Long.class
             );
 
-            step("РџСЂРѕРІРµСЂСЏРµРј, С‡С‚Рѕ РІ РѕС‚РІРµС‚ РѕС‚ Р·Р°РїСЂРѕСЃР° СЃ РїРѕРјРѕС‰СЊСЋ POST /person РїРѕР»СѓС‡Р°РµРј id РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ", () -> {
+            step("Проверяем, что в ответ от запроса с помощью POST /person получаем id пользователя", () -> {
                 assertNotNull(createPersonResponse.getBody());
             });
-            step("РћС‚РїСЂР°РІР»СЏРµРј GET person/{id} РїРѕ СЃРѕР·РґР°РЅРЅРѕРјСѓ id РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ", () -> {
+            step("Отправляем GET person/{id} по созданному id пользователя", () -> {
                 String getUrl = "http://localhost:8080/api/person/%s".formatted(createPersonResponse.getBody());
                 ResponseEntity<Person> getResponse = restTemplate.getForEntity(getUrl, Person.class);
 
-                step("РџСЂРѕРІРµСЂСЏРµРј, С‡С‚Рѕ Р·Р°РїСЂРѕСЃ РІРѕР·РІСЂР°С‰Р°РµС‚ id Рё name РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ", () -> {
+                step("Проверяем, что запрос возвращает id и name пользователя", () -> {
                     assertEquals(HttpStatus.OK, getResponse.getStatusCode());
 
                     Person retrievedPerson = getResponse.getBody();
@@ -66,13 +66,13 @@ public class GetPersonTests extends IntegrationTest {
     }
 
     @Test
-    @DisplayName("РџРѕР»СѓС‡РµРЅРёРµ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ РїРѕ СЃСѓС‰РµСЃС‚РІСѓСЋС‰РµРјСѓ id")
+    @DisplayName("Получение пользователя по существующему id")
     @AllureId("2")
     public void GetPersonWithAnotherId() {
 
         String getUrl = "http://localhost:8080/api/person/";
 
-        step("РЎРѕР·РґР°РµРј РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ СЃ РїРѕРјРѕС‰СЊСЋ РјРµС‚РѕРґР° POST /person Рё СЃРѕС…СЂР°РЅСЏРµРј РµРіРѕ id", () -> {
+        step("Создаем пользователя с помощью метода POST /person и сохраняем его id", () -> {
             String postUrl = "http://localhost:8080/api/person";
 
             Person person = new Person("Ben");
@@ -91,11 +91,11 @@ public class GetPersonTests extends IntegrationTest {
 
             long id = createPersonResponse.getBody();
 
-            step("РЈРґР°Р»СЏРµРј РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ СЃ РїРѕРјРѕС‰СЊСЋ РјРµС‚РѕРґР° DELETE /person/{id}", () -> {
+            step("Удаляем пользователя с помощью метода DELETE /person/{id}", () -> {
                 String deleteUrl = getUrl + id;
                 restTemplate.delete(deleteUrl);
 
-                step("РџСЂРѕРІРµСЂСЏРµРј, С‡С‚Рѕ РјРµС‚РѕРґ GET /person/{id} РІРѕР·СЂР°С‰Р°РµС‚ РѕС€РёР±РєСѓ 404", () -> {
+                step("Проверяем, что метод GET /person/{id} возращает ошибку 404", () -> {
                     assertThrows(FeignException.NotFound.class, () -> testPersonClient.findById(id));
                 });
             });
